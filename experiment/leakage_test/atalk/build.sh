7fb90b3210369a501151e6eb8bcb3aa0ecbcec4e
Dan Blackwell <dblackwell@viru10.viru10>
2022-09-15 14:00:06 +0100

    Move experiment down one directory

---
+++ experiment/leakage_test/atalk/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CFLAGS=""
+if ! [[ -z "${VANILLA}" ]]; then
+  CFLAGS="-D VANILLA_AFL"
+  export AFL_USE_MSAN=1
+fi
+
+gcc -O3 -c ../memory.c -o m.o
+gcc -O3 -c ../decode_inputs.c -o d.o
+gcc -O3 -c ../base64.c -o b.o
+gcc -O3 -c ../json.c -o j.o
+afl-clang-fast $CFLAGS repo/atalk.c m.o d.o b.o j.o -I../ -Irepo/ -lm -o fuzz
+
+rm *.o
---
+++ experiment/run_experiment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $(id -u) -ne 0 ]]; then 
+  echo "Please run as root"
+  exit
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+# VANILLA_AFL=1
+
+if [[ -z "${VANILLA_AFL}" ]]; then
+  echo "Building AFL_info_leakage experiment"
+  OUTPUT_DIR="../experimental_results/$(hostname)_$(date -d "today" +"%Y_%m_%d_%H%M")"
+else
+  echo "Building VANILLA AFL experiment"
+  OUTPUT_DIR="../experimental_results_vanilla/$(hostname)_$(date -d "today" +"%Y_%m_%d_%H%M")"
+fi
+mkdir -p $OUTPUT_DIR
+
+NAME="leaks_exp"
+if [[ -z "${VANILLA_AFL}" ]]; then
+  IMAGE=leaks
+  docker build -t $IMAGE .
+else
+  IMAGE=leaks_vanilla
+  docker build --build-arg VANILLA=1 -t $IMAGE .
+fi
+docker container run --name $NAME $IMAGE
+docker cp $NAME:/app/leakage_test $OUTPUT_DIR/ && docker rm $NAME
+
+chown -R dblackwell $OUTPUT_DIR
+
+echo "COMPLETED EXPERIMENT - COPIED RESULTS INTO $OUTPUT_DIR"
