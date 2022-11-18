3d7c03b350923505984e844578223336fd957ae1
Dan Blackwell <dblackwell@signal.bio>
2022-11-18 15:35:23 +0000

    Added model check all subjects script

---
+++ experiment/model_check_all_subjects.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] || [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` [max_number_of_seconds_to_model_check_for]"
+  exit 0
+fi
+
+echo "WILL BEGIN MODEL CHECKING WITH TIMEOUT OF $(( $1 / 3600 )) hours"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+for F in leakage_test/*/; do
+  pushd $F;
+    if ! [[ -f model_check.sh ]]; then
+      popd
+      continue
+    fi
+
+    echo "Beginning model checking $(basename $F)"
+    timeout $1 ./model_check.sh
+  popd
+done
+
+wait
