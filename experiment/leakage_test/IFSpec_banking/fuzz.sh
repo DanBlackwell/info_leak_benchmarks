efa9789d0457c6eb60bcf1faa28f098efd29a139
Dan Blackwell <dblackwell@viru6.viru6>
2022-12-07 18:27:30 +0000

    Fix the copying commands on run completion

--- experiment/leakage_test/IFSpec_banking/fuzz.sh
+++ experiment/leakage_test/IFSpec_banking/fuzz.sh
@@ -9,7 +9,7 @@ fi
 /usr/bin/time -v timeout $1 afl-fuzz -i IN -o SECURE_OUT -- ./fuzz_secure > fuzzing_secure.log 2>&1 &
 /usr/bin/time -v timeout $1 afl-fuzz -i IN -o INSECURE_OUT -- ./fuzz_insecure > fuzzing_insecure.log 2>&1 &
 
-cp fuzzing_secure.log ../results/$(basename pwd)_secure_fuzzing.log
-cp fuzzing_insecure.log ../results/$(basename pwd)_insecure_fuzzing.log
+cp fuzzing_secure.log ../results/$(basename $PWD)_secure_fuzzing.log
+cp fuzzing_insecure.log ../results/$(basename $PWD)_insecure_fuzzing.log
 
 wait
--- experiment/leakage_test/atalk/model_check.sh
+++ experiment/leakage_test/atalk/model_check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 /usr/bin/time -v timeout $1 cbmc test > model_check.log 2>&1
-cp model_check.log ../results/$(basename pwd)_model_check.log
+cp model_check.log ../results/$(basename $PWD)_model_check.log
