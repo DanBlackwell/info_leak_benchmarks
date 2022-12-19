5158a0ed7b1c0e8e35be90dda05c48340deae47c
Dan Blackwell <danblackwell95@yahoo.co.uk>
2022-12-19 11:21:43 +0000

    Fixed postgres build issues caused by permission problems and stored build logs for each SUT

--- experiment/leakage_test/IFSpec_reviewers/fuzz.sh
+++ experiment/leakage_test/IFSpec_reviewers/fuzz.sh
@@ -6,8 +6,9 @@ if ! [[ $1 =~ $re ]] || [ $# -ne 1 ]; then
   exit 0
 fi
 
-/usr/bin/time -v timeout $1 afl-fuzz -i IN -o SECURE_OUT -- ./fuzz_secure @@ > fuzzing_secure.log 2>&1
-/usr/bin/time -v timeout $1 afl-fuzz -i IN -o INSECURE_OUT -- ./fuzz_insecure @@ > fuzzing_insecure.log 2>&1
+/usr/bin/time -v timeout $1 afl-fuzz -i IN -o SECURE_OUT -- ./fuzz_secure @@ > fuzzing_secure.log 2>&1 &
+/usr/bin/time -v timeout $1 afl-fuzz -i IN -o INSECURE_OUT -- ./fuzz_insecure @@ > fuzzing_insecure.log 2>&1 &
+wait
 
 cp fuzzing_secure.log ../results/$(basename $PWD)_secure_fuzzing.log
 cp fuzzing_insecure.log ../results/$(basename $PWD)_insecure_fuzzing.log
